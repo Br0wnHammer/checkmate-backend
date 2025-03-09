8004fad45637952c2a340152d50b27a915034336
Alex Holliday <ajhollid@gmail.com>
2025-03-09 14:22:01 -0700

    upsert instead of create

--- db/mongo/modules/distributedCheckModule.js
+++ db/mongo/modules/distributedCheckModule.js
@@ -1,9 +1,55 @@
 import DistributedUptimeCheck from "../../models/DistributedUptimeCheck.js";
+import { ObjectId } from "mongodb";
+
 const SERVICE_NAME = "distributedCheckModule";
 
 const createDistributedCheck = async (checkData) => {
 	try {
-		const check = await new DistributedUptimeCheck({ ...checkData }).save();
+		if (typeof checkData.monitorId === "string") {
+			checkData.monitorId = ObjectId.createFromHexString(checkData.monitorId);
+		}
+		const check = await DistributedUptimeCheck.findOneAndUpdate(
+			{
+				monitorId: checkData.monitorId,
+				city: checkData.city,
+			},
+			[
+				{
+					$set: {
+						...checkData,
+
+						responseTime: {
+							$cond: {
+								if: { $ifNull: ["$count", false] },
+								then: {
+									$round: [
+										{
+											$divide: [
+												{
+													$add: [
+														{ $multiply: ["$responseTime", "$count"] },
+														checkData.responseTime,
+													],
+												},
+												{ $add: ["$count", 1] },
+											],
+										},
+										2,
+									],
+								},
+								else: checkData.responseTime,
+							},
+						},
+						count: { $add: [{ $ifNull: ["$count", 0] }, 1] },
+					},
+				},
+			],
+			{
+				upsert: true,
+				new: true,
+				runValidators: true,
+			}
+		);
 		return check;
 	} catch (error) {
 		error.service = SERVICE_NAME;
