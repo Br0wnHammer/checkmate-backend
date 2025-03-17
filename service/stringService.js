04bd53ec6f81a9da90ee0de13505ab9cb1b8828a
Skorpios <cosmicray23@hotmail.com>
2025-03-17 14:21:57 -0700

    Added translation keys for webhook notifications and updated the string service with getters for the new translations.

--- service/stringService.js
+++ service/stringService.js
@@ -182,6 +182,22 @@ class StringService {
 		return this.translationService.getTranslation("webhookSendSuccess");
 	}
 
+	get telegramRequiresBotTokenAndChatId() {
+		return this.translationService.getTranslation("telegramRequiresBotTokenAndChatId");
+	}
+	
+	get webhookUrlRequired() {
+		return this.translationService.getTranslation("webhookUrlRequired");
+	}
+	
+	get platformRequired() {
+		return this.translationService.getTranslation("platformRequired");
+	}
+	
+	get testNotificationFailed() {
+		return this.translationService.getTranslation("testNotificationFailed");
+	}
+
 	getWebhookUnsupportedPlatform(platform) {
 		return this.translationService
 			.getTranslation("webhookUnsupportedPlatform")
