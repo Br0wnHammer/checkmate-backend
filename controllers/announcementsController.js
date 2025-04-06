bca28ac1efc3840d6af9fa3fd354754c2ad01be7
Owaise Imdad <owaiseimdad@WFX-LAP-1286.local>
2025-04-06 20:20:00 +0530

    First commit regarding the get call for all the announcements.

--- controllers/announcementsController.js
+++ controllers/announcementsController.js
@@ -5,28 +5,29 @@ const SERVICE_NAME = "announcementController";
 /**
  * Controller for managing announcements in the system.
  * This class handles the creation of new announcements.
- * 
+ *
  * @class AnnouncementController
  */
 
 class AnnouncementController {
 	constructor(db, stringService) {
 		this.db = db;
-        this.stringService = stringService;
+		this.stringService = stringService;
 		this.createAnnouncementHandler = this.createAnnouncementHandler.bind(this);
+		this.getAnnouncementsHandler = this.getAnnouncementsHandler.bind(this);
 	}
 
 	/**
-     * Handles the creation of a new announcement.
-     * 
-     * @async
-     * @param {Object} req - The request object, containing the announcement data in the body.
-     * @param {Object} res - The response object used to send the result back to the client.
-     * @param {Function} next - The next middleware function in the stack for error handling.
-     * 
-     * @returns {Promise<void>} A promise that resolves once the response is sent.
-     */
-	createAnnouncementHandler = async (req, res, next) =>{
+	 * Handles the creation of a new announcement.
+	 *
+	 * @async
+	 * @param {Object} req - The request object, containing the announcement data in the body.
+	 * @param {Object} res - The response object used to send the result back to the client.
+	 * @param {Function} next - The next middleware function in the stack for error handling.
+	 *
+	 * @returns {Promise<void>} A promise that resolves once the response is sent.
+	 */
+	createAnnouncementHandler = async (req, res, next) => {
 		const { title, message } = req.body;
 
 		if (!title || !message) {
@@ -41,14 +42,29 @@ class AnnouncementController {
 			};
 
 			const newAnnouncement = await this.db.createAnnouncement(announcementData);
-            return res.success({
+			return res.success({
 				msg: this.stringService.createAnnouncementHandler,
 				data: newAnnouncement,
 			});
 		} catch (error) {
-			next(handleError(error, SERVICE_NAME, "announcementController"));
+			next(handleError(error, SERVICE_NAME, "createAnnouncementHandler"));
 		}
-	}
+	};
+
+	/**
+	 * Handles retrieving announcements.
+	 */
+	getAnnouncementsHandler = async (req, res, next) => {
+		try {
+			const allAnnouncements = await this.db.getAnnouncements();
+			return res.success({
+				msg: this.stringService.getAnnouncementsHandler,
+				data: allAnnouncements,
+			});
+		} catch (error) {
+			next(handleError(error, SERVICE_NAME, "getAnnouncementsHandler"));
+		}
+	};
 }
 
 export default AnnouncementController;
